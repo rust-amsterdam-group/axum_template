faf84b55079a69d997573e686924ac3d01b65738
etiescu <tiernanscully@gmail.com>
2021-11-23 20:32:11 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use axum::{AddExtensionLayer, Router, routing::get};
+
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    let client = Arc::new(reqwest::Client::new());
+
+    let app = Router::new()
+        .route("/", get(routes::index))
+        .route("/search", get(routes::search))
+        .layer(AddExtensionLayer::new(client));
+
+    let address = match std::env::var("PORT") {
+        Ok(port) => SocketAddr::from(([0, 0, 0, 0], port.parse::<u16>().unwrap())),
+        Err(_e) => SocketAddr::from(([127, 0, 0, 1], 8080))
+    };
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
---
+++ src/routes/search_route.rs
@@ -0,0 +1,22 @@
+use std::sync::Arc;
+
+use axum::extract::{Extension, Query};
+use reqwest::Client;
+use serde::Deserialize;
+
+pub async fn search(info: Query<Params>, client: Extension<Arc<Client>>) -> &'static str {
+    let response = client.get(format!("https://en.wikipedia.org/wiki/{}", info.query))
+        .send()
+        .await
+        .and_then(|r| r.error_for_status());
+
+    return match response {
+        Ok(_) => "success",
+        Err(_) => "oh snap"
+    };
+}
+
+#[derive(Deserialize)]
+pub struct Params {
+    query: String,
+}
