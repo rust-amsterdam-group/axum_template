11de432f0ff73782c8ef7f5f3b23825e46502d63
etiescu <tiernanscully@gmail.com>
2021-11-23 21:32:32 +0100

    fix clippy warnings

--- src/routes/search_route.rs
+++ src/routes/search_route.rs
@@ -4,17 +4,13 @@ use axum::extract::{Extension, Query};
 use reqwest::Client;
 use serde::Deserialize;
 
-pub async fn search(info: Query<Params>, client: Extension<Arc<Client>>) -> &'static str {
-    let response = client
-        .get(format!("https://en.wikipedia.org/wiki/{}", info.query))
+pub async fn search(params: Query<Params>, client: Extension<Arc<Client>>) -> &'static str {
+    client
+        .get(format!("https://en.wikipedia.org/wiki/{}", params.query))
         .send()
         .await
-        .and_then(|r| r.error_for_status());
-
-    return match response {
-        Ok(_) => "success",
-        Err(_) => "oh snap",
-    };
+        .and_then(|r| r.error_for_status())
+        .map_or("oh snap!", |_| "success")
 }
 
 #[derive(Deserialize)]
